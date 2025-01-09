a388b9fd04d5a94be419cb1dbef5ad20dea8169d
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2025-01-09 16:30:06 +0800

    批量模式直接写入进度到终端

--- PDFDeSecure/Program.cs
+++ PDFDeSecure/Program.cs
@@ -1,16 +1,16 @@
-﻿using PdfSharp.Pdf.IO;
-using PdfSharp.Pdf;
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace PDFDeSecure
 {
     static class Program
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern int FreeConsole();
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -22,22 +22,23 @@ namespace PDFDeSecure
             if (Args.Length > 2)
             {
                 //Auto Processing Mode
-                PdfDocument pdf = new PdfDocument();
-                PdfDocument outpdf = new PdfDocument();
+                PdfDocument pdf;
+                PdfDocument outpdf;
                 //First Argu Is INPUT Dir, Second Is OUTPUT Dir
                 var Input = Args[1];
                 var Output = Args[2];
-                DirectoryInfo di = new DirectoryInfo(Input);
+                DirectoryInfo di = new(Input);
                 var aryFi = di.GetFiles("*.pdf");
                 var counter = 0;
                 var error = 0;
                 foreach (FileInfo fi in aryFi)
                 {
+                    Console.WriteLine("Processing " + fi.Name);
                     //Skip file with errors
                     try
                     {
                         outpdf = new PdfDocument();
-                        Stream fileStream = fi.OpenRead();
+                        FileStream fileStream = fi.OpenRead();
                         pdf = PdfReader.Open(fileStream, PdfDocumentOpenMode.Import);
                         foreach (PdfPage page in pdf.Pages)
                         {
@@ -52,14 +53,15 @@ namespace PDFDeSecure
                     catch (Exception ex)
                     {
                         error++;
-                        File.WriteAllText(Output + "\\Error-" + fi.Name + ".log", ex.ToString());
+                        Console.Write(ex.ToString());
                     }
                 }
-                MessageBox.Show("Unlocked " + counter + " files" + Environment.NewLine + "Failed " + error + " files" + Environment.NewLine + "Percentage " + counter + "/" + (counter + error) + " = " + ((float)counter / (float)(counter + error) * 100).ToString("f2") + "%, Cheers!", "PDF file Unlocked! and Saved!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Console.WriteLine("Unlocked " + counter + " files" + Environment.NewLine + "Failed " + error + " files" + Environment.NewLine + "Percentage " + counter + "/" + (counter + error) + " = " + (counter / (float)(counter + error) * 100).ToString("f2") + "%, Cheers!", "PDF file Unlocked! and Saved!");
                 Environment.Exit(0);
             }
             else
             {
+                FreeConsole();
                 Application.EnableVisualStyles();
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new PDFDeSecure());
